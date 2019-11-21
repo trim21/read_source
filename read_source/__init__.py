6486f43edcd26a4386b71bc0e14ac97e38550869
Trim21 <i@trim21.me>
2019-11-21 20:24:59 +0800

    update readme and add new function to open file

--- read_source/__init__.py
+++ read_source/__init__.py
@@ -1,12 +1,32 @@
+import contextlib
 import re
+import itertools
 
-pattern = re.compile(r'^[ \t\f]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)')
+_pattern = re.compile(r'^[ \t\f]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)')
 
 
-def read(file_path: str):
+def get_encoding(file_path: str):
     with open(file_path, 'rb') as f:
-        lines = f.readlines()
-        if len(lines) >= 2:
-            first, second, *_ = lines
-        length = len(f.readlines())
-        first = f.readline()
+        source_lines = itertools.islice(f.readlines(), 2)
+    for line in source_lines:
+        line = line.decode('ascii')
+        result = _pattern.search(line)
+        if result:
+            return result.groups()[0]
+
+    return 'utf-8'
+
+
+@contextlib.contextmanager
+def read(path: str, mode='r'):
+    with open(
+        path,
+        mode=mode,
+        encoding=get_encoding(path),
+    ) as f:
+        yield f
+
+
+if __name__ == '__main__':
+    with read('./read_source/__init__.py') as f:
+        print(f.readlines())
--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
+# Read Python3 Source File With Correct Encoding
 
-https://www.python.org/dev/peps/pep-0263/# read_source
+according to <https://www.python.org/dev/peps/pep-0263/>,
+python3 source file encoding are default to be `utf-8`.
+
+But `open()`'s encoding will be `gbk` on windows,
+So don't use `open` without encoding to read a python3 source file. 
