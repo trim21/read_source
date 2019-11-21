16865493f71751296734b56ee598f49eaa9f519c
Trim21 <i@trim21.me>
2019-11-21 20:32:06 +0800

    update readme

--- read_source/__init__.py
+++ read_source/__init__.py
@@ -1,32 +1,26 @@
-import contextlib
 import re
 import itertools
 
-_pattern = re.compile(r'^[ \t\f]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)')
+_pattern = re.compile(rb'^[ \t\f]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)')
 
 
 def get_encoding(file_path: str):
     with open(file_path, 'rb') as f:
+        # get first line in file
         source_lines = itertools.islice(f.readlines(), 2)
     for line in source_lines:
-        line = line.decode('ascii')
+        # line = line.decode('ascii')
         result = _pattern.search(line)
         if result:
-            return result.groups()[0]
+            return result.groups()[0].decode('ascii')
 
+    # fall back to default encoding
     return 'utf-8'
 
 
-@contextlib.contextmanager
 def read(path: str, mode='r'):
-    with open(
+    return open(
         path,
         mode=mode,
         encoding=get_encoding(path),
-    ) as f:
-        yield f
-
-
-if __name__ == '__main__':
-    with read('./read_source/__init__.py') as f:
-        print(f.readlines())
+    )
--- readme.md
+++ readme.md
@@ -5,3 +5,14 @@ python3 source file encoding are default to be `utf-8`.
 
 But `open()`'s encoding will be `gbk` on windows,
 So don't use `open` without encoding to read a python3 source file. 
+
+
+example:
+
+```python
+from read_source import get_encoding, read
+print(get_encoding('tests/source/gb18030/dash-star-dash.py')) # gb18030
+with read('tests/source/gb18030/dash-star-dash.py') as f:
+    print(f.read()) 
+    # -*- coding: gb18030 -*-
+```
